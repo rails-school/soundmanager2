711c75400857d0d63c8d92796824ed8995564c37
Michael Makarov <info1117@gmail.com>
2013-12-17 14:25:56 -0800

    New App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Radio::Application.config.secret_key_base = '5c722590e4939873191fb1c4ff7690343aff822952ad0e0d8d0905f6fcd67731352c95ea34e3911faf0606ab830223eb6592dfeeb23f63f0d61a1d840e8ee403'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Radio::Application.config.session_store :cookie_store, key: '_radio_session'
